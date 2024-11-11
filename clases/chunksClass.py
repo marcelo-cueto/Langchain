a536c3ee1df5f69573360cc5f43d7b39a1857259
marcelo-cueto <marce.8706@gmail.com>
2024-11-11 12:09:11 -0300

    creacion de clases

---
+++ clases/chunksClass.py
@@ -0,0 +1,121 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, SemanticChunker, Language, MarkdownTextSplitter, PythonCodeTextSplitter
+from langchain_openai.embeddings import OpenAIEmbedding
+
+from llama_index.core import Document
+from llama_index.core.node_parser import SentenceSplitter,MarkdownNodeParser,CodeSplitter,SemanticSplitterNodeParser
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+class Chunker:
+    def __init__ (self, framework:str, provider:str='HF')->None:
+        # provider= HF | OAI
+        # HF=> HuggingFace free model
+        # OAI => OpenAI pay model
+        # framework= langchain | llamaindex
+
+        self.framework=framework
+        self.provider=provider
+
+    def character_chunker(self, chunk_size:int,chunk_overlap:int, text:str ,separators=['\n\n', '\n', ' ', ''] ):
+
+        if self.framework=='langchain':
+
+            text_aplitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=separators)
+            nodes=text_aplitter.create_documents([text])
+        else :
+            node_parser = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
+            nodes = node_parser.get_nodes_from_documents(
+                [Document(text=text)], show_progress=False
+            )
+
+        return nodes
+    
+    def document_specific_chunker(self, chunk_size:int, chunk_overlap:int, text:str):
+
+        if self.framework=='langchain':
+
+            splitter=MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+
+            return splitter.create_documents([text])
+        else:
+            parser = MarkdownNodeParser()
+
+            return parser.get_nodes_from_documents(text)
+
+    def python_chunker(self, text):
+
+        if self.framework=='langchain':
+            splitter=PythonCodeTextSplitter()
+
+            return splitter.create_documents([text])
+        else:
+            splitter = CodeSplitter(
+            language="python",
+            chunk_lines=40,  # lines per chunk
+            chunk_lines_overlap=15,  # lines overlap between chunks
+            max_chars=1500,  # max chars per chunk
+        )
+        nodes = splitter.get_nodes_from_documents(text)
+    
+
+    def language_spliter(self, language, text):
+
+#           CPP = 'cpp'
+            # GO = 'go'
+            # JAVA = 'java'
+            # KOTLIN = 'kotlin'
+            # JS = 'js'
+            # TS = 'ts'
+            # PHP = 'php'
+            # PROTO = 'proto'
+            # PYTHON = 'python'
+            # RST = 'rst'
+            # RUBY = 'ruby'
+            # RUST = 'rust'
+            # SCALA = 'scala'
+            # SWIFT = 'swift'
+            # MARKDOWN = 'markdown'
+            # LATEX = 'latex'
+            # HTML = 'html'
+            # SOL = 'sol'
+            # CSHARP = 'csharp'
+            # COBOL = 'cobol'
+            # C = 'c'
+            # LUA = 'lua'
+            # PERL = 'perl'
+            # HASKELL = 'haskell'
+            # ELIXIR = 'elixir'
+            # POWERSHELL = 'powershell'}
+        if self.framework=='langchain':
+            splitter=RecursiveCharacterTextSplitter.from_language(language=Language.JS, chunk_size=450, chunk_overlap=4)
+
+            return splitter.create_documents([text]) 
+        else:
+            splitter = CodeSplitter(
+                language=language,
+                chunk_lines=40,  # lines per chunk
+                chunk_lines_overlap=15,  # lines overlap between chunks
+                max_chars=1500,  # max chars per chunk
+            )
+            return splitter.get_nodes_from_documents(text)
+        
+    
+    def semantic_chunker(self,text, breakpoint_threshold_type:str):       
+        if self.framework=='langchain':
+            #breakpoint_threshold_type:
+            #  percentile=The default way to split is based on percentile. In this method, all differences between sentences are calculated, and then any difference greater than the X percentile is split.The default way to split is based on percentile. In this method, all differences between sentences are calculated, and then any difference greater than the X percentile is split.
+            #  standard_deviation= In this method, any difference greater than X standard deviations is split
+            #  interquartile= In this method, the interquartile distance is used to split chunks.
+            if self.provider=='HF':
+                text_splitter = SemanticChunker(HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"))
+            else:
+                text_splitter = SemanticChunker(OpenAIEmbedding())
+            return  text_splitter.create_documents([text] , breakpoint_threshold_type=breakpoint_threshold_type)
+        else :
+            if self.provider=='HF':
+                embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+            else:   
+                embed_model = OpenAIEmbedding()
+            splitter = SemanticSplitterNodeParser(
+                buffer_size=1, breakpoint_percentile_threshold=95, embed_model=embed_model
+                )
+            return splitter.get_nodes_from_documents(text)
---
+++ clases/embbedigsClass.py
@@ -0,0 +1,149 @@
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader
+from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+
+
+class Embbedings:
+
+    def __init__(self, provider:str, framework:str, api_key=None , device:str='cpu', normalize_embeddings:bool=False) ->None:
+
+
+        # provider= HF | OAI
+        # HF=> HuggingFace free model
+        # OAI => OpenAI pay model
+        #
+        # framework= langchain | llamaindex
+        #
+        # api_key= openAi api key ->only OAI
+
+
+        ################################### Hugging Face parameters#######################################
+        #         param cache_folder: str | None = None
+        # Path to store models. Can be also set by SENTENCE_TRANSFORMERS_HOME environment variable.
+
+        # param encode_kwargs: Dict[str, Any] [Optional]
+        # Keyword arguments to pass when calling the encode method for the documents of the Sentence Transformer model, such as prompt_name, prompt, batch_size, precision, normalize_embeddings, and more. See also the Sentence Transformer documentation: https://sbert.net/docs/package_reference/SentenceTransformer.html#sentence_transformers.SentenceTransformer.encode
+
+        # param model_kwargs: Dict[str, Any] [Optional]
+        # Keyword arguments to pass to the Sentence Transformer model, such as device, prompts, default_prompt_name, revision, trust_remote_code, or token. See also the Sentence Transformer documentation: https://sbert.net/docs/package_reference/SentenceTransformer.html#sentence_transformers.SentenceTransformer
+
+        # param model_name: str = 'sentence-transformers/all-mpnet-base-v2'
+        # Model name to use.
+
+        # param multi_process: bool = False
+        # Run encode() on multiple GPUs.
+
+        # param query_encode_kwargs: Dict[str, Any] [Optional]
+        # Keyword arguments to pass when calling the encode method for the query of the Sentence Transformer model, such as prompt_name, prompt, batch_size, precision, normalize_embeddings, and more. See also the Sentence Transformer documentation: https://sbert.net/docs/package_reference/SentenceTransformer.html#sentence_transformers.SentenceTransformer.encode
+
+        # param show_progress: bool = False
+        # Whether to show a progress bar.
+
+        # async aembed_documents(texts: list[str]) → list[list[float]]
+        # Asynchronous Embed search docs.
+
+        # Parameters
+        # :
+        # texts (list[str]) – List of text to embed.
+
+        # Returns
+        # :
+        # List of embeddings.
+
+        # Return type
+        # :
+        # list[list[float]]
+
+        # async aembed_query(text: str) → list[float]
+        # Asynchronous Embed query text.
+
+        # Parameters
+        # :
+        # text (str) – Text to embed.
+
+        # Returns
+        # :
+        # Embedding.
+
+        # Return type
+        # :
+        # list[float]
+
+        # embed_documents(texts: List[str]) → List[List[float]][source]
+        # Compute doc embeddings using a HuggingFace transformer model.
+
+        # Parameters
+        # :
+        # texts (List[str]) – The list of texts to embed.
+
+        # Returns
+        # :
+        # List of embeddings, one for each text.
+
+        # Return type
+        # :
+        # List[List[float]]
+
+        # embed_query(text: str) → List[float][source]
+        # Compute query embeddings using a HuggingFace transformer model.
+
+        # Parameters
+        # :
+        # text (str) – The text to embed.
+
+        # Returns
+        # :
+        # Embeddings for the text.
+
+        # Return type
+        # :
+        # List[float]
+        ##################################################################################################################
+
+
+        self.provider=provider
+        self.framework=framework
+        self.device={'device': device}
+        self.normalize_embeddings={'normalize_embeddings':normalize_embeddings}
+        
+
+        if self.provider=='HF':
+            if self.framework=='langchain':
+
+                self.embeddings = HuggingFaceEmbeddings(
+                    model_name="sentence-transformers/all-mpnet-base-v2"
+                    model_kwargs=self.device
+                    encode_kwargs=self.normalize_embeddings
+                    )
+            else:
+                Settings.embed_model = HuggingFaceEmbedding(
+                    model_name="BAAI/bge-small-en-v1.5"
+                    )
+        elif provider=='OAI':
+            self.embeddings = OpenAIEmbeddings(api_key=api_key)
+            # global
+            Settings.embed_model = OpenAIEmbedding()
+
+    def apply_embeding_document(self,text):
+        if self.framework=='langchain':
+            
+            return self.embeddings.embed_documents([text])
+        
+
+
+
+
+            
+
+
+        
+
+
+
+    
+
+
+        
